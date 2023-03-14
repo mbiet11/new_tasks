3dba5966a5a9cffa04f96d4bc6e4340fe90d8a41
Mbiet <mbietuko@udel.edu>
2023-03-14 16:47:03 -0400

    finished task 8

--- src/App.tsx
+++ src/App.tsx
@@ -16,28 +16,6 @@ function App(): JSX.Element {
             </header>
             <hr></hr>
             <h2>Hello World</h2>
-            <div>
-                Some Things I love:
-                <ul>
-                    <li> Soccer </li>
-                    <li> Bread </li>
-                    <li> Afrobeats </li>
-                </ul>
-                <div className="con">
-                    <Container>
-                        <Row>
-                            <Col>
-                                Chelsea is the best sports team in the world.
-                            </Col>
-                            <Col>They are 2x UCL winners and 6x Pl winners</Col>
-                        </Row>
-                    </Container>
-                </div>
-                <Button onClick={() => console.log("Hello World!")}>
-                    Log Hello World
-                </Button>
-            </div>
-            <img src={require("./images/Chelsea.jpg")} alt="Chelsea FC Logo" />
 
             <Counter></Counter>
             <hr />
--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -7,10 +7,22 @@ import { Button } from "react-bootstrap";
  * module (which returns a random decimal between 0 up until 1) in order
  * to produce a random integer between 1 and 6 (inclusive).
  */
+
 export function d6(): number {
     return 1 + Math.floor(Math.random() * 6);
 }
 
 export function TwoDice(): JSX.Element {
-    return <div>Two Dice</div>;
+    const [D1, setD1] = useState<number>(3);
+    const [D2, setD2] = useState<number>(5);
+
+    return (
+        <div>
+            <span data-testid="left-die">{D1}</span>
+            <span data-testid="left-die">{D2}</span>
+            <Button onClick={() => setD1(d6)}> Roll Left</Button>
+            <Button onClick={() => setD2(d6)}> Roll Right</Button>
+            {D1 === 1 && D2 === 1 ? <span> lose</span> : <span> Win</span>}
+        </div>
+    );
 }
