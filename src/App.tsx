ae863b1a883f7d63060c93307f788fd5da493cd5
Mbiet <mbietuko@udel.edu>
2023-03-28 03:03:49 -0400

    uncommented doublehalf

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App(): JSX.Element {
                 UD CISC275 with React Hooks and TypeScript by Mbiet Uko.
             </header>
             <hr></hr>
-            {/* <DoubleHalf></DoubleHalf> */}
+            {<DoubleHalf></DoubleHalf>}
             <hr></hr>
             <ChooseTeam></ChooseTeam>
             <hr></hr>
